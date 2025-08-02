9e40b5c323e3e64bba259128174f814685ec1c2d
Gamja <71741826+Gamja3288@users.noreply.github.com>
2025-08-03 06:57:11 +0900

    EMO-1 init init commit
    EMO-1

--- Assets/Scripts/EmotionManager.cs
+++ Assets/Scripts/EmotionManager.cs
@@ -5,7 +5,7 @@ public class EmotionManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        print("asdfasg");
+        print("asdgerergfasg");
     }
 
     // Update is called once per frame
